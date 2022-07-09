d6a1392d4e34be19bac8cbb56d1bbc5cb5b2b58d
QuantuPL <Quantu.Jaworski@gmail.com>
2022-07-09 12:04:50 +0200

    prototyp muzeum

--- Assets/Scripts/Interactable/RoomEdit.cs
+++ Assets/Scripts/Interactable/RoomEdit.cs
@@ -2,18 +2,43 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
+using TMPro;
+
 public class RoomEdit : MonoBehaviour, IInteractible
 {
+    [SerializeField] private TextMeshPro ExibitionTitle;
     [SerializeField] private UnityEvent OnInteract;
+
+    private bool _enable = false;
+
+    void Start ()
+    {
+        ExibitionTitle.text = "";
+    }
 
     public string Highlight()
     {
+        if (_enable)
+        {
+            return "";
+        }
+
         return "Edit room theme";
     }
 
     public bool Interact()
     {
+        if (_enable)
+        {
+            return false;
+        }
+
         OnInteract.Invoke();
         return true;
+    }
+
+    public void EnableEdit ()
+    {
+        _enable = true;
     }
 }
