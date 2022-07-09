f4bc34437f509b24f9fc68444f1949269ad02444
QuantuPL <Quantu.Jaworski@gmail.com>
2022-07-10 00:07:54 +0200

    Liczenie Prestige

--- Assets/Scripts/Exhibition/Area/ExhibitionArea.cs
+++ Assets/Scripts/Exhibition/Area/ExhibitionArea.cs
@@ -10,6 +10,8 @@ public class ExhibitionArea : MonoBehaviour
     public List<IExhibitionObject> objectsInArea = new List<IExhibitionObject>();
     public Vector2 Area = Vector2.one;
 
+    public float Prestige;
+
     [Header("Debug")]
     public bool debugRecalculate;
     private void OnDrawGizmos()
@@ -42,5 +44,53 @@ public class ExhibitionArea : MonoBehaviour
         objectsInArea.Clear();
 
         objectsInArea.AddRange(transform.GetComponentsInChildren<IExhibitionObject>());
+
+        // Calc Prestige
+
+        Dictionary<string, int> tags = new Dictionary<string, int>();
+
+        for (int i = 0; i < objectsInArea.Count; i++)
+        {
+            if (objectsInArea[i].GetType () != typeof (Exhibit))
+            {
+                continue;
+            }
+
+            Exhibit ex = (Exhibit)objectsInArea[i];
+
+            for (int j = 0; j < ex.Tags.Count; j++)
+            {
+                string tag = ex.Tags[j].tag;
+
+                if (!tags.ContainsKey(tag))
+                {
+                    tags.Add(tag, 1);
+                } else
+                {
+                    tags[tag]++;
+                }
+            }
+        }
+
+        Prestige = 0;
+        for (int i = 0; i < objectsInArea.Count; i++)
+        {
+            if (objectsInArea[i].GetType() != typeof(Exhibit))
+            {
+                continue;
+            }
+
+            Exhibit ex = (Exhibit)objectsInArea[i];
+            int maxVal = 0;
+            for (int j = 0;j < ex.Tags.Count; j++)
+            {
+                int v = tags[ex.Tags[j].tag];
+                if (v > maxVal)
+                {
+                    maxVal = v;
+                }
+            }
+            Prestige += maxVal - 1;
+        }
     }
 }
