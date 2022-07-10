326a56c07e3181454a32b3853a26f5cf184fb541
Mateusz Gawlik-Kałużny <66747844+define-laavi@users.noreply.github.com>
2022-07-10 10:53:42 +0200

    hint

---
+++ Assets/KeymasterSpeedrun.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class KeymasterSpeedrun : MonoBehaviour
+{
+    public CanvasGroup fadeOut;
+    private bool isFadingOut;
+
+    public CanvasGroup hint;
+    private bool isHintShown = true;
+    
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            StartCoroutine(LoadMenu());
+        }
+        else if (Input.GetKeyDown(KeyCode.H))
+        {
+            if (!isHintShown)
+            {
+                hint.DOFade(1, 0.5f);
+            }
+            isHintShown = true;
+            
+        }
+    }
+
+    public void HideHint()
+    {
+        hint.DOFade(0, 0.5f);
+        isHintShown = false;    
+    }
+    
+    IEnumerator LoadMenu()
+    {
+        if (!isFadingOut)
+        {
+            isFadingOut = true;
+            fadeOut.DOFade(1, 0.5f);
+            yield return new WaitForSeconds(0.5f);
+            SceneManager.LoadScene("MainMenu");
+        }
+    }
+}
